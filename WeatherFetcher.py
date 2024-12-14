2f5f1faf7a0babeefb30ab411ba4a0fb406e4d45
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2024-12-14 01:19:54 +0100

    Improve data fetching logic from API

--- WeatherFetcher.py
+++ WeatherFetcher.py
@@ -13,18 +13,12 @@ class WeatherFetcher:
                     if city.lower() in station["nazwa_stacji"].lower():
                         if station.get('temperatura_gruntu', None) is not None:
                             return_dictionary["Temperatura"] = VoiceGenerator.number_to_text(station['temperatura_gruntu'] + " stopni Celsjusza")
-                        else:
-                            return_dictionary["Temperatura"] = "Brak danych"
 
                         if station.get('wiatr_srednia_predkosd', None) is not None:
                             return_dictionary["Wiatr"] = VoiceGenerator.number_to_text(station['wiatr_srednia_predkosd'] + " metrów na sekundę")
-                        else:
-                            return_dictionary["Wiatr"] = "Brak danych"
 
                         if station.get('opad_10min', None) is not None:
                             return_dictionary["Opad"] = VoiceGenerator.number_to_text(station['opad_10min'] + " milimetrów")
-                        else:
-                            return_dictionary["Opad"] = "Brak danych"
 
                 return return_dictionary
             return None
